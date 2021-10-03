66d690b293f973cac2ff6dc2801896d80375fae1
life-tm <life_rm@rambler.ru>
2021-10-03 23:32:51 +0300

    task_5_2.py not

--- exercises/05_basic_scripts/task_5_2.py
+++ exercises/05_basic_scripts/task_5_2.py
@@ -30,3 +30,27 @@ Out[1]: '11111111111111111111111111110000'
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+network_mask = input ("Введите IP-сеть в формате: 10.1.1.0/24\n")
+network_mask = network_mask.split('/')
+network = network_mask[0].split('.')
+network_1 = int(network[0])
+network_2 = int(network[1])
+network_3 = int(network[2])
+network_4 = int(network[3])
+mask_prefix = network_mask[1]
+mask_int = '1'*int(mask_prefix)
+mask_1 = int(mask_int[0:8],2)
+mask_2 = int(mask_int[8:16],2)
+mask_3 = int(mask_int[16:23],2)
+mask_4 = int(mask_int[23:31],2)
+print('''
+Network:
+{network[0]:8}  {network[1]:8}  {network[2]:8}  {network[3]:8}
+{network_1:08b}  {network_2:08b}  {network_3:08b}  {network_4:08b}
+
+Mask:
+/{mask_prefix}
+{mask_1:<8}  {mask_2:<8} {mask_3:<8}
+{mask_1:<08b}  {mask_2:<08b} {mask_3:<08b}
+'''.format(network=network,network_1=network_1,network_2=network_2,network_3=network_3,network_4=network_4,mask_prefix=mask_prefix,mask_1=mask_1,mask_2=mask_2,mask_3=mask_3))
