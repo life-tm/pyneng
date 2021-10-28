380e8a00967906d33e5ae434aee5f2495c87af10
life-tm <life_rm@rambler.ru>
2021-10-28 18:40:15 +0300

    added tast 6.2

--- exercises/06_control_structures/task_6_2.py
+++ exercises/06_control_structures/task_6_2.py
@@ -12,3 +12,16 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+ip=input("Введите ip в формате 10.0.0.1: ")
+octets=ip.split(".")
+if int(octets[0]) > 0 and int(octets[0]) < 224:
+   print("unicast")
+elif int(octets[0]) > 223 and int(octets[0]) < 240:
+   print("multicast")
+elif "255.255.255.255" in ip:
+   print("local broadcast")
+elif "0.0.0.0" in ip:
+   print("unassigned")
+else:
+   print("unused")
